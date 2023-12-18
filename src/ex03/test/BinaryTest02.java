152a938da8a88fde2cb9f69465f066af903fa58a
yeeun0118s <yeeun0118s@naver.com>
2023-12-18 14:12:12 +0900

    이진 ã연습 완료

--- src/ex03/test/BinaryTest02.java
+++ src/ex03/test/BinaryTest02.java
@@ -3,42 +3,41 @@ package ex03.test;
 public class BinaryTest02 {
     public static void main(String[] args) {
 
-        // 시간 복잡도 log2(n) -> log2(13) -> 3,
-        //이진 검색 ==> 반드시 정렬이 되어 있어야 한다.
-        int[]arr = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // 9/2 = 4 4번 인덱스를 찾을 8과 비교해보고 작으니 4번인덱스 뒤에 있는 기준점을 기준으로
-                                                // 찾는다.
-
+        // N = 13
+        // 시간복잡도 log2(N) -> log2(13) -> 3.700 (3과 4사이)
+        // 이진 검색 => 반드시 정렬이 되어 있어야 한다.
+        // 16 / 2*2*2*2 -> logn -> log16
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         int N = arr.length;
+        final int target = 5;
+        int start = 0;
+        int end = N - 1;
+        int mid;
 
-        final int target = 8;
-        int start = 0;//인덱스
-        int end = N - 1;//인덱스
-        int mid = (end - start) / 2;//인덱스
+        int round = 1;
+        while (true) {
+            // 1 회전
+            mid = start + ((end - start) / 2); // 기대값 6
 
-        //1회전
-        if (arr[mid]==target) {
-            System.out.println(target + "값은" + mid + "번지에 있습니다.");
+            if (arr[mid] == target) {
+                System.out.println(round + "회전 : " + target + "값은 " + mid + "번지에 있습니다");
+                break;
+            }
+
+            if (arr[mid] < target) { // 기대값 start 5, end 8
+                start = mid + 1;
+            }
+
+            if (arr[mid] > target) {
+                end = mid - 1;
+            }
+
+            System.out.println(round + "회전 start : " + start);
+            System.out.println(round + "회전 end : " + end);
+
+            round++;
         }
 
-        {
-        if (arr[mid] <  target) { // 기대값 start 5, end 8
-        start = mid + 1;
-        }
-
-        if (arr[mid] > target) { // 기대값 start 0, end 3
-        end = mid - 1;
-        }
-
-        System.out.println(start);
-        System.out.println(end);
-
-        }
-        if (arr[mid] > target){
-
-        }
-
-
-
-
+        System.out.println("시간복잡도 : " + (Math.log(N) / Math.log(2)));
     }
 }
