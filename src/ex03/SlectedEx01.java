ff49b9d38475f05a9779637d7124304bd2810182
yeeun0118s <yeeun0118s@naver.com>
2023-12-18 10:38:14 +0900

    선ã택정ã렬

---
+++ src/ex03/SlectedEx01.java
@@ -0,0 +1,31 @@
+package ex03;
+
+public class SlectedEx01 {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 2, 4, 3};
+        final int N = arr.length;
+        int rep;
+        int min;
+
+        for(int j = 0; j<N-1; j++)
+            rep = 0;
+            min = rep;
+
+        for (int i = rep + 1; i < N; i++) {
+            if (arr[min] > arr[i]) { // 5, 8, 2, 4, 3
+                min = i;
+            }
+
+
+            if (rep != min) {
+                int temp = arr[rep]; // temp = 5
+                arr[rep] = arr[min];
+                arr[min] = temp;
+                }
+            }
+        for(int v : arr){
+            System.out.println(v + " ");
+
+        }
+    }
+}
