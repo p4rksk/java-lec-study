d1b7be9910774cfec73552e6879d1aa45db7b8c0
yeeun0118s <yeeun0118s@naver.com>
2023-12-27 11:57:22 +0900

    컴상속 및 생성자 컴 í ì 및 컴포지션(has)

--- src/ex06/example7/BuggerSet.java
+++ src/ex06/example7/BuggerSet.java
@@ -1,6 +1,6 @@
 package ex06.example7;
 
-public class BuggerSet { // has Burger, Coke    컴포지션 코드: 상속이 아닌 잘만들어진 코드를 사용하고 싶을 때
+public class BuggerSet { // has Burger, Coke 컴포지션 코드: 상속이 아닌 잘만들어진 코드를 사용하고 싶을 때
     private Burger burger;
     private Coke coke;
 
