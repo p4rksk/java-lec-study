0cffe4ca36ae832fd365941ed0fa535477552b8b
yeeun0118s <yeeun0118s@naver.com>
2023-12-29 21:29:16 +0900

    Scope, Que, Stack 수정

--- src/ex04/test/ScopeEx01.java
+++ src/ex04/test/ScopeEx01.java
@@ -1,19 +1,17 @@
-package ex04.test;
+package ex04.TEST;
 
 public class ScopeEx01 {
 
-    int n1 = 1;// heap에 저장
     static int n2= 2;// static에 저장
+    int n1 = 1;// heap에 저장
 
     static void m1(){//메서드 내에 만들어지는 변수를 지역변수
-        int n5 = 10; // m1 메서드를 호출해야지 m1내부인 n1의 값이 뜸 stack에 저장
+        int n5 = 10; // m1 메서드를 호출해야지 m1내부인 n5의 값이 뜸 stack에 저장
+         String a = "b";
         System.out.println("m1 : " + n5);
     }
 
-    void m2(){
-        System.out.println("m2 : " + n1);
 
-    }
     //메인 시작전에 static에 m1 메소드와 n2 변수가 로드 되어 있음.
     public static void main(String[] args) {
         System.out.println(1);
@@ -23,4 +21,13 @@ public class ScopeEx01 {
         System.out.println(sc.n1);
         sc.m2();
     }
-}
+
+    void m2() {
+        System.out.println("m2 : " + n1);
+
+    }
+
+
+
+    }
+
