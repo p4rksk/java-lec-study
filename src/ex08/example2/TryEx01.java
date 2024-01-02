0db653a740604cfcfe7170af2a692cec423ee86b
yeeun0118s <yeeun0118s@naver.com>
2024-01-02 17:52:29 +0900

    예외처리

---
+++ src/ex08/example2/TryEx01.java
@@ -0,0 +1,20 @@
+package ex08.example2;
+
+class Cal2{
+    //RuntimeException = 엄마가 알려주지 않았을 때
+    public void divde(int num) throws Exception { //메서드 앞에 throws를 걸면 호출자 할때 예외 제어권을 준다.
+        System.out.println(10/num);
+    }
+}
+
+public class TryEx01 {
+    public static void main(String[] args) {
+        Cal2 c2 = new Cal2();
+        try {
+            c2.divde(0);
+        } catch (Exception e) {
+            System.out.println("0으로 나눌 수 없어요");
+        }
+
+    }
+}
---
+++ src/ex08/example2/TryEx02.java
@@ -0,0 +1,9 @@
+package ex08.example2;
+
+
+
+public class TryEx02 {
+    public static void main(String[] args) {
+        throw new RuntimeException("오류 강제 발생");// 오류를 강제로 발생시킬때 trow new를 쓴다
+    }
+}
